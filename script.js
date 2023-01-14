f4b43e2ece096f9f6acc33ed99a4fa25f1710a05
jd-norton <jacob.daniel.norton@gmail.com>
2023-01-14 11:29:43 -0600

    intial commit. have display and button layout. goofy css

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// if (navigator.serviceWorker) {
+//     navigator.serviceWorker.register (
+//         '/color-progressive/sw.js',
+//         {scope: '/color-progressive/'}
+//     )
+// }
+
+const box = document.getElementById("box");
+// const height = box.clientHeight;
+// const width = box.clientWidth;
