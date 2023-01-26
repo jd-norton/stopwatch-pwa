4f0c437db46436753fbe1418c7de62f8c2340241
jd-norton <jacob.daniel.norton@gmail.com>
2023-01-25 19:47:11 -0600

    Implemented buttons. Timer working correctly. Got time displaying nicely. got mobile sizing right.

--- script.js
+++ script.js
@@ -7,5 +7,42 @@
 // }
 
 const box = document.getElementById("box");
-// const height = box.clientHeight;
-// const width = box.clientWidth;
+const secondsDisplay = document.getElementById("seconds");
+const tensDisplay = document.getElementById("tens");
+const buttonStart = document.getElementById("start");
+const buttonStop = document.getElementById("stop");
+const buttonReset = document.getElementById("reset");
+
+var interval;
+var seconds = 0;
+var tens = 0;
+
+buttonStart.onclick = function () {
+    clearInterval(interval);
+    interval = setInterval(increment, 10);
+}
+
+buttonStop.onclick = function () {
+    clearInterval(interval);
+}
+
+buttonReset.onclick = function () {
+    clearInterval(interval);
+    seconds = 0;
+    tens = 0;
+    updateTimer();
+}
+
+function increment() {
+    tens++;
+    if (tens >= 100) {
+        seconds++;
+        tens = 0;
+    }
+    updateTimer();
+}
+
+function updateTimer() {
+    tensDisplay.innerHTML = tens >= 10 ? tens : "0" + tens;
+    secondsDisplay.innerHTML = seconds >= 10 ? seconds : "0" + seconds;
+}
